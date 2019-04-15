5a465350d2c32a98c0526f847b9e29d2bb81e041
lcruz <lucio_cruz@discoverybrasil.com>
2019-04-15 09:36:35 -0300

    Esqueci

---
+++ app/Management.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Management extends Model{
+
+	public function insert($con,$table,$columns,$values){
+
+		$insert = "INSERT INTO $table ($columns) VALUES ($values)";		
+
+		if($con->query($insert) === true){
+			$rtr["bool"] = true;
+			$rtr["msg"] = "A New record on the table $table was successfully created!";
+		}else{
+			$rtr["bool"] = false;
+			$rtr["msg"] = "Error: ".$insert."<br>".$con->error;
+		}
+
+		return $rtr;
+
+	}
+
+	public function get($con,$parameters,$something,$table,$where,$order){
+
+		$sql = "SELECT $something FROM $table ";
+
+		if($where){
+			$sql .= "WHERE $where ";
+		}  
+
+		if($order){
+			$sql .= "ORDER BY $order";
+		}
+
+		$result = $con->query($sql);
+
+		if($result && $result->num_rows > 0){
+			$count = 0;
+			while ($row = $result->fetch_assoc()) {
+				for ($p=0; $p < sizeof($parameters); $p++) { 
+					$array[$count][$parameters[$p]] = $row[$parameters[$p]];
+				}
+				
+				$count++;
+			}
+		}else{
+			$array = FALSE;
+		}
+
+		return $array;
+
+	}
+}
