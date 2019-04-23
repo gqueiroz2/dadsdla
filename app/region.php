8173c499db2880700fbd5accda1cf83f8965629a
vballabenute <vitor_ballabenute@discoverybrasil.com>
2019-04-23 09:23:26 -0300

    merged dataManagement V6

--- app/region.php
+++ app/region.php
@@ -43,7 +43,7 @@ class region extends Management{
     }
 
     public function editRegion($con){
-
+        $sql = new sql();
         $size = intval(Request::get("size"));
         $table = "region";
         $columns = array("name");
@@ -52,17 +52,33 @@ class region extends Management{
             $old[$i] = Request::get("Old-$i");
             $new[$i] = array(Request::get("New-$i"));
 
-            $set[$i] = $this->setUpdate($columns,$new[$i]);
+            $division[$i] = Request::get("New-$i");
+
+            $set[$i] = $sql->setUpdate($columns,$new[$i]);
 
             $where[$i] = "WHERE name = '$old[$i]'";
         }
 
         for ($i=0; $i <$size ; $i++) { 
-            $bool = $this->updateValues($con,$table,$set[$i],$where[$i]);
+            
+            if ($division[$i] != null) {
+                $bool = $sql->updateValues($con,$table,$set[$i],$where[$i]);
+            }else{
+                $bool = $this->deleteRegion($con,$table,$where[$i]);
+            }
+
             if ($bool["bool"] == false) {
                 break;
             }
         }
+
+        return $bool;
+    }
+
+    public function deleteRegion($con,$table,$where){
+        $sql = new sql();
+
+        $bool = $sql->deleteValues($con,$table,$where);
 
         return $bool;
     }
