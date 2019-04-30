596b014eda4750453a6c80e07e29e554ad7f1b63
gcosta1 <guilherme_costa@discoverybrasil.com>
2019-04-30 15:20:54 -0300

    YoY v1

--- app/sql.php
+++ app/sql.php
@@ -14,7 +14,7 @@ class sql extends Model{
         return $res;
     }
 
-    public function selectSum($con,$sum,$as, $table, $join = null, $where = null, $order_by = 1, $limit = false){
+    public function selectSum($con, $sum, $as, $table, $join = null, $where = null, $order_by = 1, $limit = false){
         $sql = "SELECT SUM($sum) AS $as FROM $table $join $where ORDER BY $order_by ";
         $res = $con->query($sql);
         return $res;
