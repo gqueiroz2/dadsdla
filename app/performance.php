3c0294829e34fa7e07702ca5656797881fa630a2
gcosta1 <guilherme_costa@discoverybrasil.com>
2019-05-23 07:47:10 -0300

    fix

--- app/performance.php
+++ app/performance.php
@@ -15,8 +15,7 @@ use Illuminate\Support\Facades\Request;
 class performance extends Model{
     
     public function makeCore($con){
-    	
-        $b = new brand();
+    	$b = new brand();
         $r = new region();
         $base = new base();
         $sql = new sql();
@@ -47,11 +46,9 @@ class performance extends Model{
         }else{
             $valueView = "Net";
         }
-
         //year view
-        $yearView = $year;
+        $yearView = $year[0];
     	
-
     	//nome da região na view
     	$tmp = array($region);
         $regionView = $r->getRegion($con,$tmp)[0]["name"];
@@ -62,19 +59,34 @@ class performance extends Model{
             array_push($brandName, $brand[$b][1]);
         }
 
+
         //define de onde vai se tirar as informações do banco, sendo as opções ytd(IBMS), cmaps, header ou digital.
         $actualMonth = date("m");
         for ($b=0; $b <sizeof($brand); $b++) {
             for ($m=0; $m <sizeof($month) ; $m++) {
-                if ($brand[$b][1] == "ONL" || $brand[$b][1] == "VIX") {
-                    $sourceBrand[$b][$m] = "Digital";
+                if ($m > $actualMonth-1) {
+                    if($brand[$b][1] == "ONL" || $brand[$b][1] == "VIX") {
+                        $sourceBrand[$b][$m] = "Digital";
+                    }elseif ($region == "1") {
+                        $sourceBrand[$b][$m] = "CMAPS";
+                    }else{
+                        $sourceBrand[$b][$m] = "Header";
+                    }
                 }else{
-                    $sourceBrand[$b][$m] = "IBMS";
+                    if ($brand[$b][1] == "ONL" || $brand[$b][1] == "VIX") {
+                        $sourceBrand[$b][$m] = "Digital";
+                    }elseif ($brand[$b][1] == "OTH") {
+                        $sourceBrand[$b][$m] = "IBMS";
+                    }elseif($brand[$b][1] == "FN" && $region == "1"){
+                        $sourceBrand[$b][$m] = "CMAPS";
+                    }else{
+                        $sourceBrand[$b][$m] = $source;
+                    }
                 }
             }
         }
 
-		//olha quais nucleos serão selecionados
+		//olha quais nucleos serão selecionados        
         $salesRepName = array();
         if ($salesRepGroup == 'all') {
                 
@@ -105,6 +117,7 @@ class performance extends Model{
 
         }
 
+
         //pega informações dos representantes do nucleo(s)
         $srAllInfo = $sr->getSalesRepFilteredYear($con,$salesRepGroup,$region,$year,$source);
         $salesRep = array();
@@ -113,6 +126,7 @@ class performance extends Model{
             array_push($salesRep, $srAllInfo[$i]["id"]);
             array_push($salesRepName, $srAllInfo[$i]["salesRep"]);
         }
+
 
         for ($b=0; $b <sizeof($sourceBrand); $b++) { 
             for ($m=0; $m <sizeof($sourceBrand[$b]) ; $m++) { 
@@ -138,193 +152,119 @@ class performance extends Model{
 
         $mtx = $this->assembler($values,$planValues,$srAllInfo,$month,$brand,$salesGroup);
 
+
         return $mtx;
     }
 
     public function assembler($values,$planValues,$salesRep,$month,$brand,$salesGroup){
-        $base = new base();
 
         $tmp1["values"] = array();
         $tmp1["planValues"] = array();
         $tmp2["values"] = array();
         $tmp2["planValues"] = array();
 
-        
-
-        for ($b=0; $b <sizeof($brand); $b++) { 
-            for ($m=0; $m <sizeof($month); $m++) { 
-                for ($s=0; $s <sizeof($salesRep); $s++) { 
-                    $tmp[$s][$b][$m] = 0; 
-                    $tmp_2[$s][$b][$m] = 0; 
-                }
-            }
-        }
-
-        for ($b=0; $b <sizeof($brand); $b++) { 
-            for ($m=0; $m <sizeof($month); $m++) { 
-                for ($s=0; $s <sizeof($salesRep) ; $s++) { 
-                    $tmp[$s][$b][$m] = $values[$b][$m][$s]; 
-                    $tmp_2[$s][$b][$m] = $planValues[$b][$m][$s]; 
-                }
-            }
-        }
-
-        $values = $tmp;
-        $planValues = $tmp_2;
-
-
         $mtx["oldValues"] = $values;
-        $mtx["oldPlanValues"] = $planValues;
+        $mtx["oldPlanValues"] = $values;
         $mtx["salesRep"] = $salesRep;
         $mtx["salesGroup"] = $salesGroup;
         $mtx["brand"] = $brand;
-        $mtx["tier"] = $base->brandToTier($brand);
-        $mtx["quarters"] = $base->monthToQuarter($month);
+        $mtx["tier"] = array("T1","T2","T3");
+        $mtx["quarters"] = array("Q1","Q2","Q3","Q4");
 
-
-        for ($s=0; $s <sizeof($values);  $s++) { 
-            for ($b=0; $b <sizeof($values[$s]) ; $b++) { 
-                for ($m=0; $m <sizeof($values[$s][$b]) ; $m++) { 
-                    $oldVarAbs[$s][$b][$m] = 0;
-                    $oldVarPrc[$s][$b][$m] = 0;
-                }
+        //Começa o Agrupamento por Brand em tier
+        for ($m=0; $m <sizeof($month) ; $m++) { 
+            for ($s=0; $s <sizeof($salesRep) ; $s++) { 
+                $tmp1["values"][0][$m][$s] = 0;
+                $tmp1["values"][1][$m][$s] = 0;
+                $tmp1["values"][2][$m][$s] = 0;
+                $tmp1["planValues"][0][$m][$s] = 0;
+                $tmp1["planValues"][1][$m][$s] = 0;
+                $tmp1["planValues"][2][$m][$s] = 0;
             }
         }
 
-        for ($s=0; $s <sizeof($values) ; $s++) { 
-            for ($b=0; $b <sizeof($values[$s]) ; $b++) { 
-                for ($m=0; $m <sizeof($values[$s][$b]) ; $m++) { 
-                    $oldVarAbs[$s][$b][$m] = $values[$s][$b][$m] - $planValues[$s][$b][$m];
-                    if ($planValues[$s][$b][$m] != 0) {
-                        $oldVarPrc[$s][$b][$m] = $values[$s][$b][$m] / $planValues[$s][$b][$m];
+        for ($b=0; $b <sizeof($brand); $b++) { 
+            for ($m=0; $m <sizeof($month) ; $m++) { 
+                for ($s=0; $s <sizeof($salesRep) ; $s++) { 
+                    if ($brand[$b][1] == 'DC' || $brand[$b][1] == 'HH' || $brand[$b][1] == 'DK') {
+                        $tmp1["values"][0][$m][$s] += $values[$b][$m][$s];
+                        $tmp1["planValues"][0][$m][$s] += $planValues[$b][$m][$s];
+                    }elseif ($brand[$b][1] == 'AP' || $brand[$b][1] == 'TLC' || $brand[$b][1] == 'ID' || $brand[$b][1] == 'DT' || $brand[$b][1] == 'FN' || $brand[$b][1] == 'ONL') {
+                        $tmp1["values"][1][$m][$s] += $values[$b][$m][$s];
+                        $tmp1["planValues"][1][$m][$s] += $planValues[$b][$m][$s];
                     }else{
-                        $oldVarPrc[$s][$b][$m] = 0;
+                        $tmp1["values"][2][$m][$s] += $values[$b][$m][$s];
+                        $tmp1["planValues"][2][$m][$s] += $planValues[$b][$m][$s];
                     }
                 }
             }
         }
+        //terminou de Agrupar por Tier
 
-        $mtx["oldVarAbs"] = $oldVarAbs;
-        $mtx["oldVarPrc"] = $oldVarPrc;
-
-        //começa a agrupar por sales Group
-        for ($sg=0; $sg <sizeof($salesGroup) ; $sg++) {
-            for ($b=0; $b <sizeof($brand); $b++) { 
-                for ($m=0; $m <sizeof($month); $m++) { 
-                    $tmp1["values"][$sg][$b][$m] = 0;
-                    $tmp1["planValues"][$sg][$b][$m] = 0;
-                }
+        //Começou a agrupar os meses em Quarter
+        for ($t=0; $t <sizeof($tmp1["values"]) ; $t++) { 
+            for ($s=0; $s <sizeof($salesRep) ; $s++) { 
+                $tmp2["values"][$t][0][$s] = 0;
+                $tmp2["planValues"][$t][0][$s] = 0;
+                $tmp2["values"][$t][1][$s] = 0;
+                $tmp2["planValues"][$t][1][$s] = 0;
+                $tmp2["values"][$t][2][$s] = 0;
+                $tmp2["planValues"][$t][2][$s] = 0;
+                $tmp2["values"][$t][3][$s] = 0;
+                $tmp2["planValues"][$t][3][$s] = 0;
             }
         }
 
-        for ($sg=0; $sg <sizeof($salesGroup) ; $sg++) {
-            for ($s=0; $s <sizeof($salesRep); $s++) { 
-                if ($salesRep[$s]["salesRepGroup"] == $salesGroup[$sg]["name"]) {
-                    for ($b=0; $b <sizeof($brand); $b++) { 
-                        for ($m=0; $m <sizeof($month); $m++) { 
-                            $tmp1["values"][$sg][$b][$m] += $values[$s][$b][$m];
-                            $tmp1["planValues"][$sg][$b][$m] += $planValues[$s][$b][$m];
-                        }
-                    }
-                }
-            }
-        }
-        //Termina de Agrupar
-        
-        //Começou a agrupar por tier
-        for ($sg=0; $sg <sizeof($salesGroup); $sg++) { 
-            for ($t=0; $t <sizeof($mtx["tier"]); $t++) { 
-                for ($m=0; $m <sizeof($month); $m++) { 
-                    $tmp2["values"][$sg][$t][$m] = 0;
-                    $tmp2["planValues"][$sg][$t][$m] = 0;
-                }
-                $mtx["totalValueTier"][$sg][$t] = 0;
-                $mtx["totalPlanValueTier"][$sg][$t] = 0;
-            }
-        }
 
-        for ($sg=0; $sg <sizeof($salesGroup); $sg++) { 
-            for ($t=0; $t <sizeof($mtx["tier"]); $t++) { 
-                for ($m=0; $m <sizeof($month); $m++) { 
-                    for ($b=0; $b <sizeof($brand); $b++) { 
-                        if (($brand[$b][1] == 'DC' || $brand[$b][1] == 'HH' || $brand[$b][1] == 'DK') && $mtx["tier"][$t] == "T1") {
-                            $tmp2["values"][$sg][$t][$m] += $tmp1["values"][$sg][$b][$m];
-                            $tmp2["planValues"][$sg][$t][$m] += $tmp1["planValues"][$sg][$b][$m];
-                            $mtx["totalValueTier"][$sg][$t] += $tmp1["values"][$sg][$b][$m];
-                            $mtx["totalPlanValueTier"][$sg][$t] += $tmp1["planValues"][$sg][$b][$m];
-                        }elseif($brand[$b][1] == 'OTH' && $mtx["tier"][$t] == "T3"){
-                            $tmp2["values"][$sg][$t][$m] += $tmp1["values"][$sg][$b][$m];
-                            $tmp2["planValues"][$sg][$t][$m] += $tmp1["planValues"][$sg][$b][$m];
-                            $mtx["totalValueTier"][$sg][$t] += $tmp1["values"][$sg][$b][$m];
-                            $mtx["totalPlanValueTier"][$sg][$t] += $tmp1["planValues"][$sg][$b][$m];
-                        }elseif($mtx["tier"][$t] == "T2"){
-                            $tmp2["values"][$sg][$t][$m] += $tmp1["values"][$sg][$b][$m];
-                            $tmp2["planValues"][$sg][$t][$m] += $tmp1["planValues"][$sg][$b][$m];
-                            $mtx["totalValueTier"][$sg][$t] += $tmp1["values"][$sg][$b][$m];
-                            $mtx["totalPlanValueTier"][$sg][$t] += $tmp1["planValues"][$sg][$b][$m];
-                        }
-                    }
-                }
-            }
-        }
-        //terminou
-        //Começou a agrupar mes
-        for ($sg=0; $sg <sizeof($salesGroup) ; $sg++) { 
-            for ($t=0; $t <sizeof($mtx["tier"]) ; $t++) { 
-                for ($q=0; $q <sizeof($mtx["quarters"]) ; $q++) { 
-                    $mtx["value"][$sg][$t][$q] = 0;
-                    $mtx["planValue"][$sg][$t][$q] = 0;
-                }
-            }
-        }
-
-        for ($sg=0; $sg <sizeof($salesGroup) ; $sg++) { 
-            for ($t=0; $t <sizeof($mtx["tier"]) ; $t++) { 
-                for ($q=0; $q <sizeof($mtx["quarters"]) ; $q++) { 
-                    for ($m=0; $m <sizeof($month) ; $m++) { 
-                        if(($month[$m] == '1' || $month[$m] == '2'|| $month[$m] == '3' ) && $mtx["quarters"][$q] == "Q1"){
-                            $mtx["value"][$sg][$t][$q] += $tmp2["values"][$sg][$t][$m];
-                            $mtx["planValue"][$sg][$t][$q] += $tmp2["planValues"][$sg][$t][$m];
-                        }elseif(($month[$m] == '4' || $month[$m] == '5' || $month[$m] == '6') && $mtx["quarters"][$q] == "Q2") {
-                            $mtx["value"][$sg][$t][$q] += $tmp2["values"][$sg][$t][$m];
-                            $mtx["planValue"][$sg][$t][$q] += $tmp2["planValues"][$sg][$t][$m];
-                        }elseif(($month[$m] == '7' || $month[$m] == '8' || $month[$m] == '9' ) && $mtx["quarters"][$q] == "Q3") {
-                            $mtx["value"][$sg][$t][$q] += $tmp2["values"][$sg][$t][$m];
-                            $mtx["planValue"][$sg][$t][$q] += $tmp2["planValues"][$sg][$t][$m];
-                        }elseif(($month[$m] == '10' || $month[$m] == '11' || $month[$m] == '12' ) && $mtx["quarters"][$q] == "Q4"){
-                            $mtx["value"][$sg][$t][$q] += $tmp2["values"][$sg][$t][$m];
-                            $mtx["planValue"][$sg][$t][$q] += $tmp2["planValues"][$sg][$t][$m];
-                        }
-
-                    } 
-                }
-            }
-        }
-        //terminou
-
-        for ($sg=0; $sg <sizeof($mtx["value"]); $sg++) { 
-            for ($t=0; $t <sizeof($mtx["value"][$sg]); $t++) { 
-                for ($q=0; $q <sizeof($mtx["value"][$sg][$t]); $q++) { 
-                    $mtx["varAbs"][$sg][$t][$q] = $mtx["value"][$sg][$t][$q] - $mtx["planValue"][$sg][$t][$q]; 
-                    if ($mtx["planValue"][$sg][$t][$q] != 0) {
-                        $mtx["varPrc"][$sg][$t][$q] = $mtx["value"][$sg][$t][$q] / $mtx["planValue"][$sg][$t][$q];
+        //t de tier
+        for ($t=0; $t <sizeof($tmp1["values"]) ; $t++) { 
+            for ($m=0; $m <sizeof($month) ; $m++) { 
+                for ($s=0; $s <sizeof($salesRep) ; $s++) { 
+                    if ($month[$m] == "1" || $month[$m] == "2" || $month[$m] == "3") {
+                        $tmp2["values"][$t][0][$s] += $tmp1["values"][$t][$m][$s];
+                        $tmp2["planValues"][$t][0][$s] += $tmp1["planValues"][$t][$m][$s];
+                    }elseif($month[$m] == "4" || $month[$m] == "5" || $month[$m] == "6") {
+                        $tmp2["values"][$t][1][$s] += $tmp1["values"][$t][$m][$s];
+                        $tmp2["planValues"][$t][1][$s] += $tmp1["planValues"][$t][$m][$s];
+                    }elseif($month[$m] == "7" || $month[$m] == "8" || $month[$m] == "9") {
+                        $tmp2["values"][$t][2][$s] += $tmp1["values"][$t][$m][$s];
+                        $tmp2["planValues"][$t][2][$s] += $tmp1["planValues"][$t][$m][$s];
                     }else{
-                        $mtx["varPrc"][$sg][$t][$q] = 0 ;
+                        $tmp2["values"][$t][3][$s] += $tmp1["values"][$t][$m][$s];
+                        $tmp2["planValues"][$t][3][$s] += $tmp1["planValues"][$t][$m][$s];
+                    }
+                }
+            }
+        }
+        //Terminou de Agrupar em Quarter
+
+        //Começa a agrupar por SalesGroup
+
+
+        for ($t=0; $t <sizeof($tmp2["values"]) ; $t++) { 
+            for ($q=0; $q <sizeof($tmp2["values"][$t]) ; $q++) { 
+                for ($sg=0; $sg <sizeof($salesGroup); $sg++) { 
+                    $mtx["values"][$t][$q][$sg] = 0; 
+                    $mtx["planValues"][$t][$q][$sg] = 0; 
+                }
+            }
+        }
+
+        for ($t=0; $t <sizeof($tmp2["values"]) ; $t++) { 
+            for ($q=0; $q <sizeof($tmp2["values"][$t]) ; $q++) { 
+                for ($s=0; $s <sizeof($salesRep) ; $s++) { 
+                    for ($sg=0; $sg <sizeof($salesGroup) ; $sg++) { 
+                        if ($salesRep[$s]["salesRepGroup"] == $salesGroup[$sg]["name"]) {
+                            $mtx["values"][$t][$q][$sg] += $tmp2["values"][$t][$q][$s];
+                            $mtx["planValues"][$t][$q][$sg] += $tmp2["planValues"][$t][$q][$s];
+                        }
                     }
                 }
             }
         }
 
-        for ($sg=0; $sg <sizeof($mtx["value"]); $sg++) { 
-            for ($t=0; $t <sizeof($mtx["value"][$sg]); $t++) { 
-                $mtx["totalVarAbs"][$sg][$t] = $mtx["totalValueTier"][$sg][$t] - $mtx["totalPlanValueTier"][$sg][$t];
-                if ($mtx["totalPlanValueTier"][$sg][$t] != 0) {
-                    $mtx["totalVarPrc"][$sg][$t] = $mtx["totalValueTier"][$sg][$t] / $mtx["totalPlanValueTier"][$sg][$t];
-                }else{
-                    $mtx["totalVarPrc"][$sg][$t] = 0;
-                }
-            }
-        }
+        //Terminou de Agrupar em SalesGroup
+
 
         return $mtx;
     }
--- app/renderPerformance.php
+++ app/renderPerformance.php
@@ -7,57 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class renderPerformance extends Render
 {
     public function coreMatrix($mtx){
-	    for ($sg=0; $sg < sizeof($mtx["value"]); $sg++) {
-	    	if ($sg%2 == 0) {
-	    		echo "<div class='row'>";
-	    	}
-	    	echo "<div class='col'>";
-	    	echo "<table  border='1' style='width:100%; margin-top:1.5%;'><th>".$mtx["salesGroup"][$sg]["name"]."</th></table>";
-	    	for ($t=0; $t < sizeof($mtx["value"][$sg]); $t++) { 
-		    	echo "<table border='1' style='width: 100%;' class='mt-3'>";
-		    		echo "<tr>";
-		    			echo "<td rowspan='5' style='width:3%;'>".$mtx["tier"][$t]."</td>";
-		    			echo "<td style='width:7%;'></td>";
-		    			for ($m=0; $m <sizeof($mtx["quarters"]); $m++) { 
-		    				echo "<td style='width:18%;' >".$mtx["quarters"][$m]."</td>";
-		    			}
-		    			echo "<td style='width:18%;' >Total</td>";
-		    		echo "</tr>";
-		    		echo "<tr>";
-		    			echo "<td>Meta</td>";
-		    			for ($q=0; $q <sizeof($mtx["quarters"]); $q++) { 
-		    				echo "<td>".number_format($mtx["planValue"][$sg][$t][$q],0)."</td>";
-		    			}
-		    			echo "<td>".number_format($mtx["totalPlanValueTier"][$sg][$t],0)."</td>";
-		    		echo "</tr>";
-		    		echo "<tr>";
-		    			echo "<td>Real</td>";
-		    			for ($q=0; $q <sizeof($mtx["quarters"]); $q++) { 
-		    				echo "<td>".number_format($mtx["value"][$sg][$t][$q],0)."</td>";
-		    			}
-		    			echo "<td>".number_format($mtx["totalValueTier"][$sg][$t],0)."</td>";
-		    		echo "</tr>";
-		    		echo "<tr>";
-		    			echo "<td>Var Abs</td>";
-		    			for ($q=0; $q <sizeof($mtx["quarters"]); $q++) { 
-		    				echo "<td>".number_format($mtx["varAbs"][$sg][$t][$q],0)."</td>";
-		    			}
-		    			echo "<td>".number_format($mtx["totalVarAbs"][$sg][$t],0)."</td>";
-		    		echo "</tr>";
-		    		echo "<tr>";
-		    			echo "<td>Var %</td>";
-		    			for ($q=0; $q <sizeof($mtx["quarters"]); $q++) { 
-		    				echo "<td>".number_format($mtx["varPrc"][$sg][$t][$q],0)."</td>";
-		    			}
-		    			echo "<td>".number_format($mtx["totalVarPrc"][$sg][$t],0)."%</td>";
-		    		echo "</tr>";
-		    	echo "</table>";
-	    	}
-	    	echo "</div>";
-
-	    	if ($sg%2 == 1) {
-	    		echo "</div>";
-	    	}
-	    }
+    	for ($t=0; $t < sizeof($mtx["values"]); $t++) { 
+	    	echo "<table>";
+	    		echo "<tr>";
+	    		echo "</tr>";
+	    	echo "</table>";
+    	}
     }
 }
