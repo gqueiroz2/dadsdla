39fc3b6e8657be77d44f52ab1212fd07a2a270d1
gcosta1 <guilherme_costa@discoverybrasil.com>
2019-06-18 10:47:41 -0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ Route::get('test','HomeController@test')
 Route::post('test','RootController@postTest')
 						->name('postTest');
 
-Route::get('home','adSalesController@home');
-Route::get('/','adSalesController@home');
+Route::get('home','adSalesController@home')->name('home');
+Route::get('/','adSalesController@home')->name('home');
 
 Route::group(['prefix' => 'adsales'],function(){
 	Route::get('/','adSalesController@home')
