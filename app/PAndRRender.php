d8223d4a007f1ec7a963062d1c2d58a90721347b
vballabenute <vitor_ballabenute@discoverybrasil.com>
2019-06-24 12:33:40 -0300

    Gui é fresco

---
+++ app/PAndRRender.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PAndRRender extends Render
+{
+    public function relatorio(){
+    	echo "<table>";
+    	echo "</table>";
+    }
+}
