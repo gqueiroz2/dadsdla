fc9c9653602f202e6f0c4ce4006eafdbd1895da8
lcruz <lucio_cruz@discoverybrasil.com>
2019-07-15 11:19:55 -0300

    fix

--- app/rank.php
+++ app/rank.php
@@ -197,16 +197,17 @@ class rank extends Model{
             }
 
         }else{
-
+            var_dump("ELSE");
+            var_dump($tableName);
             $tableAbv = "a";
             $leftAbv = "b";
 
             $as = "total";
 
             if ($tableName == "ytd") {
-                $value .= "_revenue";
-                $columns = array("campaign_sales_office_id", "campaign_currency_id", "brand_id", "month", "year");
-                $colsValue = array($region, $currency[0]['id'], $brands_id, $months);
+                $value .= "_revenue_prate";
+                $columns = array("sales_representant_office_id", "brand_id", "month", "year");
+                $colsValue = array($region, $brands_id, $months);
             }else{
                 $columns = array("brand_id", "month", "year");
                 $colsValue = array($brands_id, $months);
