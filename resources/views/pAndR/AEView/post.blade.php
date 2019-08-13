d7c6bacac8d46ef6a09f78ce07b4404d33fa59a9
lcruz <lucio_cruz@discoverybrasil.com>
2019-08-13 17:26:57 -0300

    fix

--- resources/views/pAndR/AEView/post.blade.php
+++ resources/views/pAndR/AEView/post.blade.php
@@ -32,6 +32,13 @@
 					@endif
 				</div>
 				<div class="col">
+					<label class='labelLeft'><span class="bold">Year:</span></label>
+					@if($errors->has('year'))
+						<label style="color: red;">* Required</label>
+					@endif
+					{{$render->year()}}
+				</div>
+				<div class="col">
 					<label class='labelLeft'><span class="bold">Sales Rep:</span></label>
 					@if($errors->has('salesRep'))
 						<label style="color: red;">* Required</label>
