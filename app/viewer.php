14ac6256f8c36ac96d29877791e8c37d5984e876
lcruz <lucio_cruz@discoverybrasil.com>
2019-10-14 10:54:24 -0300

    viewer v4

--- app/viewer.php
+++ app/viewer.php
@@ -6,21 +6,54 @@ use Illuminate\Database\Eloquent\Model;
 
 class viewer extends Model{
 
-	public function matrix($con,$salesRegion,$source,$month,$piNumber,$brand,$value,$year,$salesCurrency,$salesRep){
-		$from = ('revenue');
+	public function getTables($con,$value,$month,$source,$piNumber=NULL,$brand,$year){
 
-			for ($m=0; $m <($month); $m++) { 
-				for ($b=0; $b <($brand); $b++) { 
-					for ($s=0; $s <($salesRep); $s++){ 
-						
-					}
-				}
-			}
-<<<<<<< HEAD
-=======
+		if ($source == 'cmaps'){ 
+			$select = "SELECT sr.name, c.pi_number, c.month, b.name AS 'brand', SUM(c.'$value') AS '$value' 
+						   FROM cmaps c
+						   LEFT JOIN sales_Rep sr ON sr.ID = c.sales_rep_id
+						   LEFT JOIN sales_rep_status srs ON srs.sales_rep_id = sr.ID
+						   LEFT JOIN brand b ON b.ID = c.brand_id
+						   WHERE (c.brand_id IN ('$brand')) 
+								AND (c.year = '$year') 
+								AND (c.month IN ('$month'))
+								AND (srs.status = '1')
+						   GROUP BY c.pi_number
+						   ORDER BY c.month";
+
+			$result = $con->query($select);
+
+		}elseif ($source == 'ibms/bts'){
+			$select = "SELECT sr.name, y.order_reference,y.month,b.name, SUM(y.'$value'_revenue) AS '$value'
+					   FROM ytd y 
+					   LEFT JOIN sales_rep sr ON sr.ID = y.sales_rep_id
+					   LEFT JOIN sales_rep_status srs ON srs.sales_rep_id = sr.ID
+					   LEFT JOIN brand b ON b.ID = y.brand_id
+					   WHERE (y.brand_id IN ('brand'))
+							AND (y.year = '$year')
+							AND (y.month IN ('$month'))
+							AND (srs.status = '1')
+					   GROUP BY y.order_reference
+					   ORDER BY y.month";
+
+			$result = $con->query($select);
+
+		}elseif ($source == 'fw'){
+			$select = "SELECT sr.name, f.insertion_order,f.month,b.name,SUM(f.'$value'_revenue)
+						FROM fw_digital f
+						LEFT JOIN sales_rep sr ON sr.ID = f.sales_rep_id
+						LEFT JOIN sales_rep_status srs ON srs.sales_rep_id = sr.ID
+						LEFT JOIN brand b  ON b.ID = f.brand_id
+						WHERE (f.brand_id IN ('$brand'))
+								AND (f.year = '$year')
+								AND (f.month IN ('$month'))
+								AND (srs.status = '1')
+						GROUP BY f.insertion_order_id
+						ORDER BY f.month";
+		}elseif ($source == "sf"){
+			
 		}
-
->>>>>>> 2a5a9fa824a1a91342c96eb4a6b91f87851a0ad9
+		
 	}
 
 }
