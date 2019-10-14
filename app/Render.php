77fd70e4d6d6574e239f8054eb537e49b9ebd4c8
root <root@localhost.localdomain>
2019-10-14 16:20:47 -0300

    fix2

--- app/Render.php
+++ app/Render.php
@@ -224,12 +224,7 @@ class Render extends Model{
         $arraySource = array("CMAPS","IBMS/BTS","FW","SF");
 
          echo "<select id='sourceDataBase' name='sourceDataBase' style='width:100%;' class='form-control'>";
-            echo "<option value=''> Select </option>";
-            /*
-            for ($a=0; $a <sizeof($arraySource) ; $a++) {
-                echo "<option value='".$arraySource[$a]."'>".$arraySource[$a]."</option>";
-            }
-            */
+            echo "<option value=''> Select </option>";           
         echo "</select>";
 
     }
