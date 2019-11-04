c3088795973e171f00d00476634295c0ce976e3f
root <root@localhost.localdomain>
2019-11-04 11:25:41 -0300

    fix rankings

--- resources/views/auth/logout.blade.php
+++ resources/views/auth/logout.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center" style="margin-top: 7.5%;">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header" style="text-align: center; font-weight: bold; font-size: 20px;">You are logged off</div>
