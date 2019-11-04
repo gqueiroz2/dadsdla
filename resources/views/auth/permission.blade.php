4191e1e4043a5f4ff9514612a596766a360b5dbd
root <root@localhost.localdomain>
2019-11-04 13:01:29 -0300

    fix

--- resources/views/auth/permission.blade.php
+++ resources/views/auth/permission.blade.php
@@ -11,11 +11,8 @@
                         <div class="row justify-content-center">
                             <div class="col-6">
                                 <div>
-                                    
                                     You don't have the permission to access D|ADS, please contact your Regional Manager or send an e-mail to
-                                     
                                     <a href="mailto:d_ads@discovery.com" style="display: inline-block;" >D|ADS</a>.
-                                           
                                 </div>
                             </div>
                         </div>
