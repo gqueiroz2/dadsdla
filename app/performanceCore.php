65c7b38746441899af733de39af40f9a2a74e31b
vballabenute <vitor_ballabenute@discoverybrasil.com>
2019-11-06 12:43:44 -0300

    performance joao completo

--- app/performanceCore.php
+++ app/performanceCore.php
@@ -136,6 +136,13 @@ class performanceCore extends performance
 
 
 	public function assembler($values,$planValues,$salesRep,$month,$brand,$salesGroup,$tier,$region,$year,$currency,$valueView,$div,$divDig){
+        setlocale(LC_ALL, "en_US.utf8");
+        $uN = iconv("utf-8", "ascii//TRANSLIT", Request::session()->get('userName'));
+        $db = new dataBase();
+        $con = $db->openConnection("DLA");
+        $sql = new sql();
+
+
         $base = new base();
 
         $tmp1["values"] = array();
@@ -170,6 +177,27 @@ class performanceCore extends performance
                 }
             }    
         }
+
+        $valueFix = strtolower($valueView);
+        $from = array('revenue');
+
+        for ($s=0; $s <sizeof($salesRep) ; $s++) { 
+            for ($b=0; $b <sizeof($brand); $b++) {
+                for ($m=0; $m <sizeof($month); $m++) { 
+                    if( ($salesRep[$s]['id'] == 131) && ($m > 5) && ($brand[$b][1] == 'ONL' || $brand[$b][1] == 'VIX') ){
+                        if( $brand[$b][1] == "ONL"){
+                            $select[$b][$m] = "SELECT SUM(".$valueFix."_revenue) AS revenue FROM fw_digital WHERE (region_id = 1) AND(year = '$year') AND (brand_id != '10') AND (month = '".($m+1)."')";
+                        }elseif($brand[$b][1] == "VIX"){
+                            $select[$b][$m] = "SELECT SUM(".$valueFix."_revenue) AS revenue FROM fw_digital WHERE (region_id = 1) AND(year = '$year') AND (brand_id = '10') AND (month = '".($m+1)."')";
+                        }
+                        $result[$b][$m] = $con->query($select[$b][$m]);
+                        $kaplau = doubleval($sql->fetch($result[$b][$m],$from,$from)[0]['revenue'])*$divDig;
+                        $tmp[$s][$b][$m] = $kaplau;
+                    }                    
+                }                
+            }
+        }
+
 
         $values = $tmp;
         $planValues = $tmp_2;
@@ -665,7 +693,7 @@ class performanceCore extends performance
         }
 
         for ($sg=0; $sg <sizeof($mtx["salesGroup"]) ; $sg++) {
-            if ($salesGroup[$sg]['id'] != 30) {
+            if ($salesGroup[$sg]['id'] != 30 || $uN == 'Joao Romano') {
                 for ($b=0; $b <sizeof($mtx["brand"]); $b++) {
                     for ($m=0; $m <sizeof($mtx["month"]) ; $m++) { 
                         $mtx["total"]["case4"]["values"][$b][$m] += $mtx["case4"]["values"][$sg][$b][$m];
@@ -732,7 +760,7 @@ class performanceCore extends performance
             $mtx["total"]["case4"]["dnPlanValue"][$m] = 0;
             $mtx["total"]["case4"]["dnValue"][$m] = 0;
             for ($sg=0; $sg <sizeof($mtx["salesGroup"]) ; $sg++) { 
-                if ($salesGroup[$sg]['id'] != 30) {
+                if ($salesGroup[$sg]['id'] != 30 || $uN == 'Joao Romano') {
                     $mtx["total"]["case4"]["dnPlanValue"][$m] += $mtx["case4"]["dnPlanValue"][$sg][$m];
                     $mtx["total"]["case4"]["dnValue"][$m] += $mtx["case4"]["dnValue"][$sg][$m];
                 }
@@ -749,7 +777,7 @@ class performanceCore extends performance
             $mtx["total"]["case4"]["totalValueTier"][$b] = 0;
             $mtx["total"]["case4"]["totalPlanValueTier"][$b] = 0;
             for ($sg=0; $sg <sizeof($mtx["salesGroup"]) ; $sg++) { 
-                if ($salesGroup[$sg]['id'] != 30) {
+                if ($salesGroup[$sg]['id'] != 30 || $uN == 'Joao Romano') {
                     $mtx["total"]["case4"]["totalValueTier"][$b] += $mtx["case4"]["totalValueTier"][$sg][$b];
                     $mtx["total"]["case4"]["totalPlanValueTier"][$b] += $mtx["case4"]["totalPlanValueTier"][$sg][$b];
                 }
@@ -783,7 +811,7 @@ class performanceCore extends performance
             $mtx["total"]["case3"]["dnPlanValue"][$m] = 0;
             $mtx["total"]["case3"]["dnValue"][$m] = 0;
             for ($sg=0; $sg <sizeof($mtx["salesGroup"]) ; $sg++) { 
-                if ($salesGroup[$sg]['id'] != 30) {
+                if ($salesGroup[$sg]['id'] != 30 || $uN == 'Joao Romano') {
                     $mtx["total"]["case3"]["dnPlanValue"][$m] += $mtx["case3"]["dnPlanValue"][$sg][$m];
                     $mtx["total"]["case3"]["dnValue"][$m] += $mtx["case3"]["dnValue"][$sg][$m];
                 }
@@ -833,7 +861,7 @@ class performanceCore extends performance
             $mtx["total"]["case2"]["dnPlanValue"][$q] = 0;
             $mtx["total"]["case2"]["dnValue"][$q] = 0;
             for ($sg=0; $sg <sizeof($mtx["salesGroup"]) ; $sg++) { 
-                if ($salesGroup[$sg]['id'] != 30) {
+                if ($salesGroup[$sg]['id'] != 30 || $uN == 'Joao Romano') {
                     $mtx["total"]["case2"]["dnPlanValue"][$q] += $mtx["case2"]["dnPlanValue"][$sg][$q];
                     $mtx["total"]["case2"]["dnValue"][$q] += $mtx["case2"]["dnValue"][$sg][$q];
                 }
@@ -850,7 +878,7 @@ class performanceCore extends performance
             $mtx["total"]["case2"]["totalValueBrand"][$b] = 0;
             $mtx["total"]["case2"]["totalPlanValueBrand"][$b] = 0;
             for ($sg=0; $sg <sizeof($mtx["salesGroup"]) ; $sg++) { 
-                if ($salesGroup[$sg]['id'] != 30) {
+                if ($salesGroup[$sg]['id'] != 30 || $uN == 'Joao Romano') {
                     $mtx["total"]["case2"]["totalValueBrand"][$b] += $mtx["case2"]["totalValueBrand"][$sg][$b];
                     $mtx["total"]["case2"]["totalPlanValueBrand"][$b] += $mtx["case2"]["totalPlanValueBrand"][$sg][$b];
                 }
@@ -885,7 +913,7 @@ class performanceCore extends performance
             $mtx["total"]["case1"]["dnPlanValue"][$q] = 0;
             $mtx["total"]["case1"]["dnValue"][$q] = 0;
             for ($sg=0; $sg <sizeof($mtx["salesGroup"]) ; $sg++) { 
-                if ($salesGroup[$sg]['id'] != 30) {
+                if ($salesGroup[$sg]['id'] != 30 || $uN == 'Joao Romano') {
                     $mtx["total"]["case1"]["dnPlanValue"][$q] += $mtx["case1"]["totalPlanSG"][$sg][$q];
                     $mtx["total"]["case1"]["dnValue"][$q] += $mtx["case1"]["totalSG"][$sg][$q];
                 }
