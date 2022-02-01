beccb3ce9183dea9e028e3131f7e1c1c9d008bd6
Lucas-Regei <lucas_nova@discoverybrasil.com>
2022-02-01 15:15:08 -0300

    Relationship - Brazil/Argentina with Agency Group without Agency

--- app/relationship.php
+++ app/relationship.php
@@ -54,24 +54,37 @@ class relationship extends Model{
 
 			}
 		}else{
+			//var_dump($list);
 			for ($l=0; $l < sizeof($list); $l++) { 
 				$temp[$l] =  $ins->getAgencyByAgencyGroupID($con,$list[$l]["agencyGroupID"]);
 				$child[$l]['agencyGroup'] = $list[$l]['agencyGroup'];
-				$child[$l]['agencyGroupID'] = $list[$l]['agencyGroupID'];				
-				for ($m=0; $m < sizeof($temp[$l]); $m++) { 
-					if( !is_null($temp[$l][$m]['agency']) ){						
-						$child[$l]['agency'][$m]['name'] = $temp[$l][$m]['agency'];
-						$child[$l]['agency'][$m]['id'] = $temp[$l][$m]['agencyID'];
-						$temp2[$m] = $ins->getAgencyUnitByAgencyID($con,$child[$l]['agency'][$m]['id']);
-						if($temp2[$m]){
-							for ($n=0; $n < sizeof($temp2[$m]); $n++) { 
-								$child[$l]['agency'][$m]['agencyUnit'][$n] = $temp2[$m][$n]['agencyUnit'];
+				$child[$l]['agencyGroupID'] = $list[$l]['agencyGroupID'];
+				//var_dump($temp);
+				if (!is_array($temp[$l])) {
+					//var_dump($temp[$l]);
+					$child[$l]['agency'][0]['name'] = $temp[$l][0]['agency'];
+					$child[$l]['agency'][0]['id'] = $temp[$l][0]['agencyID'];
+					$child[$l]['agency'][0]['agencyUnit'] = false;
+					//var_dump($child[$l]);
+				} else {
+					for ($m=0; $m < sizeof($temp[$l]); $m++) {
+						//var_dump($temp[$l]); 
+						var_dump($child[$l]);
+						if( !is_null($temp[$l][$m]['agency']) ){						
+							$child[$l]['agency'][$m]['name'] = $temp[$l][$m]['agency'];
+							$child[$l]['agency'][$m]['id'] = $temp[$l][$m]['agencyID'];
+							$temp2[$m] = $ins->getAgencyUnitByAgencyID($con,$child[$l]['agency'][$m]['id']);
+							//var_dump($temp2);
+							if($temp2[$m]){
+								for ($n=0; $n < sizeof($temp2[$m]); $n++) { 
+									$child[$l]['agency'][$m]['agencyUnit'][$n] = $temp2[$m][$n]['agencyUnit'];
+								}
+							}else{
+								$child[$l]['agency'][$m]['agencyUnit'] = false;
 							}
 						}else{
-							$child[$l]['agency'][$m]['agencyUnit'] = false;
+							$child[$l]['agency'] = false;
 						}
-					}else{
-						$child[$l]['agency'] = false;
 					}
 				}
 			}
