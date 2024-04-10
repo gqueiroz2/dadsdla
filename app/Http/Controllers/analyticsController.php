ff9caa09e94a370640cecf0aa2940ff5892fbddb
gqueiroz <gabriela.queiroz@wbd.com>
2024-04-10 09:59:58 -0300

    analytics save brazil hour users

--- app/Http/Controllers/analyticsController.php
+++ app/Http/Controllers/analyticsController.php
@@ -36,6 +36,11 @@ class analyticsController extends Controller{
     	$r = new region();
     	$u = new User();
     	$at = new analytics();
+        $h = "3"; //HORAS DO FUSO ((BRASÍLIA = -3) COLOCA-SE SEM O SINAL -).
+        $hm = $h * 60;
+        $ms = $hm * 60;
+        //COLOCA-SE O SINAL DO FUSO ((BRASÍLIA = -3) SINAL -) ANTES DO ($ms). DATA
+        $gmdata = gmdate("m/d/Y", time()-($ms)); 
 
         $default = $db->defaultConnection();
         $con = $db->openConnection($default);
@@ -44,7 +49,8 @@ class analyticsController extends Controller{
     	$userRegion = Request::get('userRegion');
     	$userEmail = Request::get('userEmail');
     	$date = Request::get('date');
-    	$hour = Request::get('hour');
+    	$hour =  gmdate("H:i", time()-($ms)); 
+
     	$url = Request::get('url');
     	$shortUrl = Request::get('shortUrl');
     	$ipV1 = Request::get('ipV1');
