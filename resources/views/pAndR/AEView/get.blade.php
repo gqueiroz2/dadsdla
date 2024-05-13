57674ff32118a7f8914e126a66f996b70b09fdb3
gqueiroz <gabriela.queiroz@wbd.com>
2024-05-13 10:57:06 -0300

    font-size cicle

--- resources/views/pAndR/AEView/get.blade.php
+++ resources/views/pAndR/AEView/get.blade.php
@@ -57,7 +57,7 @@
 	</form>
 	<div class="container-fluid">
 		<div class="row justify-content-end mt-2">
-			<div class="col-3" style="color: #0070c0;font-size: 25px;">
+			<div class="col-4" style="color: #0070c0;font-size: 24px;">
 				Forecast Cicle <span style="color:red;">(Aguarde o início no próximo ciclo)</span>
 			</div>
 		</div>
